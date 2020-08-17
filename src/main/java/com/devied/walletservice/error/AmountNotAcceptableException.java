2b2f0dd7dcc4d4f3c9119c1d735ae1959e58b3fa
Andrea Caretto <andreacaretto@email.it>
2020-08-17 17:17:33 +0200

    advice controler2
    ;

--- src/main/java/com/devied/walletservice/error/AmountNotAcceptableException.java
+++ src/main/java/com/devied/walletservice/error/AmountNotAcceptableException.java
@@ -1,4 +1,9 @@
 package com.devied.walletservice.error;
 
+import com.devied.walletservice.util.Error;
+
 public class AmountNotAcceptableException extends BaseError {
+    public AmountNotAcceptableException(Error error) {
+        super(error);
+    }
 }
