62aacef0364f4f2b25d412ec61244902c08a5afe
AndreaBellone123 <andrezombiekiller646@gmail.com>
2020-08-18 15:31:50 +0200

    Added donation date

--- src/main/java/com/devied/walletservice/util/ControllerAdvisor.java
+++ src/main/java/com/devied/walletservice/util/ControllerAdvisor.java
@@ -2,6 +2,7 @@ package com.devied.walletservice.util;
 
 import com.devied.walletservice.error.BaseError;
 import com.devied.walletservice.model.ErrorEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,12 +13,9 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class ControllerAdvisor extends ResponseEntityExceptionHandler {
 
-    private BaseError baseError;
-
     @ExceptionHandler(value = {BaseError.class})
-    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
-        ErrorEntity errorEntity = new ErrorEntity(baseError.getError().getCode(), baseError.getError().getMessage());
-        // return handleExceptionInternal(ex, errorEntity , headers,  HttpStatus.CONFLICT, request);
-        return new ResponseEntity<>(errorEntity.getMessage(), HttpStatus.valueOf(errorEntity.getCode()));
+    protected ResponseEntity<Object> handleConflict(BaseError ex, WebRequest request) {
+        ErrorEntity errorEntity = new ErrorEntity(ex.getError().getCode(), ex.getError().getMessage());
+        return handleExceptionInternal(ex, errorEntity ,new HttpHeaders(),  HttpStatus.CONFLICT, request);
     }
 }
