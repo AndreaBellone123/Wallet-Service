342f5e9652820e1927815fa22a3e307dd529895b
kruuusk <andreacaretto@email.it>
2020-08-20 14:07:24 +0200

    Classe PayoutData

---
+++ src/main/java/com/devied/walletservice/data/PayoutData.java
@@ -0,0 +1,13 @@
+package com.devied.walletservice.data;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Document("payouts")
+public class PayoutData {
+
+    
+}
--- src/main/java/com/devied/walletservice/event/AnnotationDrivenListener.java
+++ src/main/java/com/devied/walletservice/event/AnnotationDrivenListener.java
@@ -1,12 +1,24 @@
 package com.devied.walletservice.event;
 
+import com.devied.walletservice.data.UserData;
+import com.devied.walletservice.error.UserNotFoundException;
+import com.devied.walletservice.repository.UserDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnnotationDrivenListener {
+
+    @Autowired
+    UserDataRepository userDataRepository;
     @EventListener
-    public void handleContextStart(CustomSpringEvent customSpringEvent) {
+    public void handleContextStart(CustomSpringEvent customSpringEvent) throws UserNotFoundException {
         System.out.println("Success");
+        UserData userData = userDataRepository.findByEmail(customSpringEvent.getDonationData().getStreamer()).orElseThrow(UserNotFoundException::new);
+        if (userData.getEarned() >= 1000){
+            //TODO CashOut
+            userData.setEarned(userData.getEarned() - 1000);
+        }
     }
 }
