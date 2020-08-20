450ac74dfd9cdd677bfb41e733f10eaaea55ec67
kruuusk <andreacaretto@email.it>
2020-08-20 14:17:20 +0200

    TODOs

--- src/main/java/com/devied/walletservice/data/PayoutData.java
+++ src/main/java/com/devied/walletservice/data/PayoutData.java
@@ -8,6 +8,24 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Setter
 @Document("payouts")
 public class PayoutData {
-
-    
+/*TODO implementare entity con attributi e repository e service eventuali
+    {
+        "sender_batch_header": {
+            "sender_batch_id": "2014021801",
+            "recipient_type": "EMAIL",
+            "email_subject": "You have money!",
+            "email_message": "You received a payment. Thanks for using our service!"
+    },
+        "items": [
+            {
+            "amount": {
+                "value": "10.00",
+                "currency": "EUR"
+            },
+            "sender_item_id": "201403140001",
+            "recipient_wallet": "PAYPAL",
+            "receiver": " receiver@example.com"
+            }
+        ]
+    }*/
 }
